9c0cbb83a55591c706ec4bab6c154fac0a1a2c8d
chenbin <chenbin@hrwuu.com>
2021-07-28 16:28:20 +0800

    init

---
+++ config.js
@@ -0,0 +1,3 @@
+(function(_this) {
+  _this.urlConfig = 'https://api.hrwuu.com';
+})(window)
---
+++ precache-manifest.c19fd1cf2f0553712cfad93f624e7b91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eca4cfd41b161e16439f0bfb66970b1",
+    "url": "/signHelper/index.html"
+  },
+  {
+    "revision": "437fa124c78f8705d412",
+    "url": "/signHelper/static/css/2.edaf9c85.chunk.css"
+  },
+  {
+    "revision": "cc783c6e1f8d4e84c8db",
+    "url": "/signHelper/static/css/main.20f4665c.chunk.css"
+  },
+  {
+    "revision": "437fa124c78f8705d412",
+    "url": "/signHelper/static/js/2.f01fd6a5.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/signHelper/static/js/2.f01fd6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc783c6e1f8d4e84c8db",
+    "url": "/signHelper/static/js/main.40d8de28.chunk.js"
+  },
+  {
+    "revision": "c61d59417717fe045986",
+    "url": "/signHelper/static/js/runtime-main.165151ac.js"
+  },
+  {
+    "revision": "dbb294ccc8ca4b336ee0b8bc913049b1",
+    "url": "/signHelper/static/media/1.dbb294cc.png"
+  },
+  {
+    "revision": "0030be437bc4701e8d010b3a83903486",
+    "url": "/signHelper/static/media/3.0030be43.png"
+  },
+  {
+    "revision": "5e675c9154bae7625dc4bc663d5fbf76",
+    "url": "/signHelper/static/media/logo.5e675c91.png"
+  },
+  {
+    "revision": "dfb6245f733d3199879840346a39a3ca",
+    "url": "/signHelper/static/media/u704.dfb6245f.png"
+  }
+]);
